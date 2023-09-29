6fc2e4df26dc2a60ee3d2accd00ba32b7c4d08e5
bvddw <galaid.arseniy@gmail.com>
2023-09-29 14:38:07 +0300

    fixed methods, added functional to the Order class

--- lesson_3/shop.cpp
+++ lesson_3/shop.cpp
@@ -6,15 +6,20 @@ class User; // Forward declaration
 
 class Product {
     public:
-        const char* productName;
-        float productPrice;
-
         Product(const char* name, float price) : 
             productName(name), productPrice(price) {}
 
         void getProductInfo() {
             cout << this->productName << " costs $" << this->productPrice << endl;
         }
+
+        float getProductPrice() {
+            return this->productPrice;
+        }
+
+    private:
+        const char* productName;
+        float productPrice;
 };
 
 class OrderDetails {
@@ -24,7 +29,7 @@ class OrderDetails {
         void addProduct(Product* product) {
             if (this->productListSize < this->maxProductCount) {
                 this->productList[this->productListSize++] = product;
-                this->totalPrice += product->productPrice;
+                this->totalPrice += product->getProductPrice();
             } else {
                 std::cout << "The cart is full. Cannot add more products." << std::endl;
             }
@@ -37,7 +42,7 @@ class OrderDetails {
             }
         }
 
-        float getTotalPrice() const {
+        float getTotalPrice() {
             return this->totalPrice;
         }
 
@@ -53,14 +58,14 @@ class ShippingInfo {
         ShippingInfo(const char* type, const char* country, float cost) :
             shippingType(type), shippingCountry(country), shippingCost(cost) {}
 
+        float getTotalPrice() {
+            return this->shippingCost;
+        }
+
         void displayShippingInfo() {
             cout << "Type: " << this->shippingType << endl;
             cout << "Country: " << this->shippingCountry << endl;
             cout << "Price: $" << this->shippingCost << endl;
-        }
-
-        float getTotalPrice() const {
-            return this->shippingCost;
         }
 
     private:
@@ -79,17 +84,19 @@ class Customer {
             return this->accountBalance;
         }
 
-        void displayCustomerInfo() {
-            cout << "Name: " << this->customerName << endl;
-            cout << "Address: " << this->customerAddress << endl;
-            cout << "Email: " << this->customerEmail << endl;
+        float checkBalance() {
+            return this->accountBalance;
         }
 
         const char* getCustomerName() const {
             return this->customerName;
         }
 
-        User registration(const char* usern, const char* pass);
+        void displayCustomerInfo() {
+            cout << "Name: " << this->customerName << endl;
+            cout << "Address: " << this->customerAddress << endl;
+            cout << "Email: " << this->customerEmail << endl;
+        }
 
     protected:
         const char* customerName;
@@ -105,10 +112,10 @@ class User : public Customer {
         const char* password;
 
     public:
-        User(const char* name, const char* address, const char* email, const char* cardNumber, const char* username, const char* password)
-            : Customer(name, address, email, cardNumber), username(username), password(password) {}
+        User(const char* name, const char* address, const char* email, const char* cardNumber, const char* username, const char* password, float balance)
+            : Customer(name, address, email, cardNumber, balance), username(username), password(password) {}
 
-        void displayUserInfo() {
+        void displayCustomerInfo() {
             cout << "Name: " << this->customerName << endl;
             cout << "Address: " << this->customerAddress << endl;
             cout << "Email: " << this->customerEmail << endl;
@@ -116,7 +123,7 @@ class User : public Customer {
             cout << "Balance: $" << this->accountBalance << endl;
         }
 
-        bool verify_login(const char* usernameToCheck, const char* passwordToCheck) {
+        bool verifyLogin(const char* usernameToCheck, const char* passwordToCheck) {
             if (strcmp(this->username, usernameToCheck) == 0 && strcmp(this->password, passwordToCheck) == 0) {
                 cout << "Success login!" << endl;
                 return true;
@@ -130,17 +137,35 @@ class User : public Customer {
         }
 };
 
-User Customer::registration(const char* usern, const char* pass) {
-    User new_user(this->customerName, this->customerAddress, this->customerEmail, this->cardNumber, usern, pass);
-    new_user.addFunds(this->accountBalance);
-    cout << "Registration complete successfully! Your login: " << usern << endl;
-    return new_user;
-}
-
 class Order {
     public:
         Order(Customer customer, const char* status, ShippingInfo shipping, OrderDetails details):
-            orderCustomer (customer), orderStatus(status), orderShipping(shipping),  orderDetails (details) {}
+        orderCustomer(customer), orderShipping(shipping), orderDetails(details) {
+            orderStatus = new char[strlen(status) + 1];
+            strcpy(this->orderStatus, status);
+        }
+
+        ~Order() {
+            delete[] orderStatus;
+        }
+
+
+        float getOrderTotalPrice() {
+            float shippingPrice = this->orderShipping.getTotalPrice();
+            float detailsPrice = this->orderDetails.getTotalPrice();
+            return shippingPrice + detailsPrice;
+        }
+
+        bool placeOrder() {
+            if (this->getOrderTotalPrice() <= this->orderCustomer.checkBalance()) {
+                strcpy(this->orderStatus, "completed");
+                cout << "Your order successfully completed!" << endl;
+                return true;
+            }
+            strcpy(this->orderStatus, "canceled");
+            cout << endl << "You do not have enough money." << endl;
+            return false;
+        }
 
         void displayOrderInfo() {
             cout << "ORDER STATUS: " << this->orderStatus << endl;
@@ -150,27 +175,31 @@ class Order {
             this->orderShipping.displayShippingInfo();
             cout << endl << "ORDER DETAILS" << endl;
             this->orderDetails.displayOrderDetails();
-            cout << endl << "TOTAL PRICE $" << this->orderDetails.getTotalPrice() + this->orderShipping.getTotalPrice() << endl;
+            cout << endl << "TOTAL PRICE $" << this->getOrderTotalPrice() << endl;
         }
 
     private:
         Customer orderCustomer;
-        const char* orderStatus;
+        char* orderStatus;
         ShippingInfo orderShipping;
         OrderDetails orderDetails;
 };
 
 int main() {
-    User user1("Jack", "NewAvenue", "test@gmail.com", "4141232311332244", "jack", "testpass");
-    Customer customer1("Bill", "OldAvenue", "email@gmail.com", "4141323233114422", 300);
-    Product product1("banana", 6);
-    Product product2("apple", 7);
-    OrderDetails cart;
-    cart.addProduct(&product1);
-    cart.addProduct(&product2);
-    ShippingInfo new_shipping("Barge", "Ukraine", 120);
+    User user1("Jack", "NewAvenue", "test@gmail.com", "4141232311332244", "jack", "testpass", 300);
+    Customer customer1("Bill", "OldAvenue", "email@gmail.com", "4141323233114422", 100);
+    // Product product1("banana", 6);
+    // Product product2("apple", 7);
+    // OrderDetails cart;
+    // cart.addProduct(&product1);
+    // cart.addProduct(&product2);
+    // ShippingInfo new_shipping("Barge", "Ukraine", 120);
     
-    Order new_order(customer1, "pending", new_shipping, cart);
-    new_order.displayOrderInfo();
+    // Order new_order(customer1, "pending", new_shipping, cart);
+    // new_order.displayOrderInfo();
+    // new_order.placeOrder();
+    // new_order.displayOrderInfo();
+    customer1.displayCustomerInfo();
+    user1.displayCustomerInfo();
     return 0;
 }
